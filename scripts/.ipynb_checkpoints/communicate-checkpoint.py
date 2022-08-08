dfee26f82e67441bfa46fd9f3a80a40c61661a6f
Jez-Carter <j.carter10@lancaster.ac.uk>
2022-08-08 13:56:16 +0100

    remove checkpoint files

--- scripts/.ipynb_checkpoints/communicate-checkpoint.py
+++
@@ -1,9 +0,0 @@
-#!/bin/env python
-
-import subprocess
-
-print(subprocess.Popen(["squeue","--user=carter10"]))
-
-process = subprocess.Popen(["squeue","--user=carter10"], stdout=subprocess.PIPE)
-
-process.communicate()
--- scripts/Jasmin_Batch_Scripts/.ipynb_checkpoints/run_all-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-python importing_and_adjusting_data_batch_script.py
-python regridding_imported_data_batch_script.py
